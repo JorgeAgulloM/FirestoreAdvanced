3fa39a64a0205699d624469cd1d62fc3059fcc7f
Jorge1 <agullojorge@gmail.com>
2024-04-10 07:36:40 +0200

    Configured binding

--- app/src/main/java/com/softyorch/firestoreadvanced/MainActivity.kt
+++ app/src/main/java/com/softyorch/firestoreadvanced/MainActivity.kt
@@ -2,12 +2,17 @@ package com.softyorch.firestoreadvanced
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.softyorch.firestoreadvanced.databinding.ActivityHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
