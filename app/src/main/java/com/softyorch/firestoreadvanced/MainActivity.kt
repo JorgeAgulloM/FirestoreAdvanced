c80df6c0a68afe6f27dc93574038184fd7cab6ba
Jorge1 <agullojorge@gmail.com>
2024-04-10 07:57:31 +0200

    implemented home view model

--- app/src/main/java/com/softyorch/firestoreadvanced/MainActivity.kt
+++ app/src/main/java/com/softyorch/firestoreadvanced/MainActivity.kt
@@ -2,17 +2,54 @@ package com.softyorch.firestoreadvanced
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.softyorch.firestoreadvanced.databinding.ActivityHomeBinding
+import com.softyorch.firestoreadvanced.domain.model.Product
+import com.softyorch.firestoreadvanced.ui.home.HomeUiState
+import com.softyorch.firestoreadvanced.ui.home.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var viewModel: HomeViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+        initUi()
+    }
+
+    private fun initUi() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect { state ->
+                    renderLastProduct(state.lastProduct)
+                    renderTopProducts(state.topProducts)
+                    renderProducts(state.products)
+                }
+            }
+        }
+    }
+
+    private fun renderLastProduct(lastProduct: Product?) {
+
+    }
+
+    private fun renderTopProducts(topProducts: List<Product>) {
+
+    }
+
+    private fun renderProducts(products: List<Product>) {
+
     }
 }
