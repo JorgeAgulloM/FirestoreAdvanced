2edf31f18d57d7cee1921d587e08be7178084cf8
Jorge1 <agullojorge@gmail.com>
2024-04-10 19:49:35 +0200

    Implemented binding and viewModel

--- app/src/main/java/com/softyorch/firestoreadvanced/ui/home/HomeActivity.kt
+++ app/src/main/java/com/softyorch/firestoreadvanced/ui/home/HomeActivity.kt
@@ -2,13 +2,51 @@ package com.softyorch.firestoreadvanced.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.softyorch.firestoreadvanced.R
+import com.softyorch.firestoreadvanced.databinding.ActivityHomeBinding
+import com.softyorch.firestoreadvanced.domain.model.Product
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var viewModel: HomeViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+        initUi()
+    }
+
+    private fun initUi() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect { state ->
+                    renderLastProduct(state.lastProduct)
+                    renderTopProducts(state.topProducts)
+                    renderProducts(state.products)
+                }
+            }
+        }
+    }
+
+    private fun renderLastProduct(lastProduct: Product?) {
+
+    }
+
+    private fun renderTopProducts(topProducts: List<Product>) {
+
+    }
+
+    private fun renderProducts(products: List<Product>) {
+
     }
 }
