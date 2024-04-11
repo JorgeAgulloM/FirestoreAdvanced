ecaaff837271678981f59ee26fde2662e37ee999
Jorge1 <agullojorge@gmail.com>
2024-04-11 18:57:50 +0200

    Modified name param

--- app/src/main/java/com/softyorch/firestoreadvanced/domain/model/Product.kt
+++ app/src/main/java/com/softyorch/firestoreadvanced/domain/model/Product.kt
@@ -2,7 +2,7 @@ package com.softyorch.firestoreadvanced.domain.model
 
 data class Product(
     val id: String,
-    val name: String,
+    val title: String,
     val description: String,
     val imageUrl: String,
     val price: String
