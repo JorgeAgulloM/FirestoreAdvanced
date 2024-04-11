4827687fc6ad9061aec654ac20c063f170b1db45
Jorge1 <agullojorge@gmail.com>
2024-04-11 19:20:31 +0200

    Shown last product

--- app/src/main/java/com/softyorch/firestoreadvanced/ui/home/HomeActivity.kt
+++ app/src/main/java/com/softyorch/firestoreadvanced/ui/home/HomeActivity.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.bumptech.glide.Glide
 import com.softyorch.firestoreadvanced.R
 import com.softyorch.firestoreadvanced.databinding.ActivityHomeBinding
 import com.softyorch.firestoreadvanced.domain.model.Product
@@ -48,7 +49,17 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun renderLastProduct(lastProduct: Product?) {
-
+        if (lastProduct == null) return
+        binding.viewLastProduct.apply {
+            lastProduct.apply {
+                tvTitle.text = title
+                tvDescription.text = description
+                Glide.with(this@HomeActivity)
+                    .load(imageUrl)
+                    .placeholder(R.drawable.ic_place_holder)
+                    .into(ivLastProduct)
+            }
+        }
     }
 
     private fun renderTopProducts(topProducts: List<Product>) {
