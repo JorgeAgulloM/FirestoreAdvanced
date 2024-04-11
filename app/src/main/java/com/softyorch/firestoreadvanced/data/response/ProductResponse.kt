e8caa923619893fff14a135bdfb70869fc2612d2
Jorge1 <agullojorge@gmail.com>
2024-04-11 19:16:57 +0200

    Modified name param

--- app/src/main/java/com/softyorch/firestoreadvanced/data/response/ProductResponse.kt
+++ app/src/main/java/com/softyorch/firestoreadvanced/data/response/ProductResponse.kt
@@ -4,12 +4,12 @@ import com.softyorch.firestoreadvanced.domain.model.Product
 
 data class ProductResponse(
     val id: String = "",
-    val name: String = "",
+    val title: String = "",
     val description: String = "",
     val imageUrl: String = "",
     val price: String = "",
 ) {
     companion object {
-        fun ProductResponse.toDomain(): Product = Product(id, name, description, imageUrl, price)
+        fun ProductResponse.toDomain(): Product = Product(id, title, description, imageUrl, price)
     }
 }
