5ea9391c5a9f3301d7f84d0b3c89ad062c7b751f
Jorge1 <agullojorge@gmail.com>
2024-04-11 19:20:17 +0200

    Getting last product

--- app/src/main/java/com/softyorch/firestoreadvanced/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/softyorch/firestoreadvanced/ui/home/HomeViewModel.kt
@@ -1,16 +1,39 @@
 package com.softyorch.firestoreadvanced.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.softyorch.firestoreadvanced.data.network.DatabaseService
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(): ViewModel() {
+class HomeViewModel @Inject constructor(private val repository: DatabaseService) : ViewModel() {
 
     private val _uiState = MutableStateFlow(HomeUiState())
     val uiState: StateFlow<HomeUiState> = _uiState
 
+    init {
+        getData()
+    }
+
+    private fun getData() {
+        getLastProduct()
+    }
+
+    private fun getLastProduct() {
+        viewModelScope.launch {
+            val response = withContext(Dispatchers.IO) {
+                repository.getLastProduct()
+            }
+
+            _uiState.update { it.copy(lastProduct = response) }
+        }
+    }
 
 }
